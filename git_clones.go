05b1c3d3fd698cad8592b41cd73ee1f80cf3ed73
solsticenet <solsticenet@mm1.myfiosgateway.com>
2020-12-10 19:36:28 -0500

    golang version

--- git_clones.go
+++ git_clones.go
@@ -14,42 +14,48 @@ import (
 )
 
 type gitClone struct {
-	baseURL  string
-	userURL  string
-	username string
+	baseURL, username string
 }
 
 func (gc gitClone) httpReq() *[]byte {
+	userURL := gc.baseURL + gc.username + "?&tab=repositories&q=&type=source"
 	client := &http.Client{}
-	req, err := http.NewRequest("GET", gc.userURL, nil)
+	req, err := http.NewRequest("GET", userURL, nil)
 	resp, err := client.Do(req)
 	if err != nil {
-		fmt.Printf("request failed: %s\n", gc.userURL)
-		os.Exit(2)
+		fmt.Println("here1")
+		panic(err)
 	}
 	rBytes, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("here2")
+		panic(err)
+	}
 	return &rBytes
 }
 
-func (gc *gitClone) setURL() {
-	gc.userURL = gc.baseURL + gc.username
-	gc.userURL += "?&tab=repositories&q=&type=source"
-}
-
-func (gc *gitClone) download(repository []byte) {
+func (gc gitClone) download(repository []byte) {
 	projectURL := gc.baseURL + gc.username
 	projectURL += fmt.Sprintf("/%s.git", string(repository))
 	fmt.Printf("Cloning %s\n", repository)
-	exec.Command("git", "clone", projectURL).Output()
+	_, err := exec.Command("git", "clone", projectURL).Output()
+	if err != nil {
+		fmt.Printf("Failed to download repository: %s\n", repository)
+	}
 }
 
-func (gc *gitClone) findRepositories(requestData []byte) {
+func (gc gitClone) findRepositories(requestData []byte) {
 	pattern := fmt.Sprintf(`<a\s?href\W+%s\S+`, gc.username)
 	result := regexp.MustCompile(pattern)
 	links := result.FindAll(requestData, -1)
 	for _, tag := range links {
 		gc.download(bytes.Split(tag[:len(tag)-1], []byte{0x2f})[2])
 	}
+}
+
+func (gc gitClone) runGitClones() {
+	pageData := gc.httpReq()
+	gc.findRepositories(*pageData)
 }
 
 func main() {
@@ -59,9 +65,13 @@ func main() {
 		fmt.Println("required argument: -u <Github username>")
 		os.Exit(1)
 	}
+	defer func() {
+		if err := recover(); err != nil {
+			fmt.Printf("Error: %v\n", err)
+			os.Exit(1)
+		}
+	}()
 
 	var gitclone = &gitClone{username: *user, baseURL: "https://github.com/"}
-	gitclone.setURL()
-	pageData := gitclone.httpReq()
-	gitclone.findRepositories(*pageData)
+	gitclone.runGitClones()
 }
